ee22e89ba0c3914c1aefeece38834cc847b93289
Harish Kota <harishkota0308@gmail.com>
2026-07-14 21:41:46 +0530

    Configure frontend to accept VITE_GOOGLE_CLIENT_ID env variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const GOOGLE_CLIENT_ID = "290960187669-jtj6rvhjs6pos6eo19fioo4r7adrmh7h.apps.googleusercontent.com" // Replace with actual Client ID
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "290960187669-jtj6rvhjs6pos6eo19fioo4r7adrmh7h.apps.googleusercontent.com"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
